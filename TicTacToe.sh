639784164e129f47f660736e1f53d8c40a445ae0
sid-thakur47 <sidthakur258@gmail.com>
2020-04-09 19:36:55 +0530

    ADDED:Function to check winner and tie

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -4,6 +4,11 @@ declare -A dictTicTacToe
 
 LAST_POSITION=9
 
+check=1
+checkTie=0
+arrRow=(1 4 7)
+arrColumn=(1 2 3)
+
 function resetBoard() {
 	for ((i=1;i<=$LAST_POSITION;i++))
 	do
@@ -23,18 +28,16 @@ function chooseSymbol() {
 		fi
 	echo -e "player symbol is:$playerSymbol\nComputer Symbol is $computerSymbol"
 }
-
 function tossForPlayer() {
 	read -p "Press Enter for toss:" key
 	rand=$(($((RANDOM%2))+1))
 		if [ $rand -eq 1 ]
 		then
 			echo "Player is playing"
-		else
+		else 
 			echo "Computer is playing"
 		fi
 }
-
 function displayGame() {
 	index=1
 	echo "---------------"
@@ -50,16 +53,69 @@ function displayGame() {
 	done
 	echo "---------------"
 }
+
 function playTicTacToe() {
 	resetBoard
 	tossForPlayer
 	chooseSymbol
-	while [[ $check -ne $LAST_POSITION ]]
+	while [[ $check -ne 0 ]]
 	do
 		read -p "Enter the position:" position
 		dictTicTacToe[$position]=$playerSymbol
 		displayGame
-		((check++))
-	done	
+		checkWinner
+		checkForTie
+		done	
 }
+
+
+function checkRow() {
+	for ((i=0;i<${#arrRow[*]};i++))
+	do
+		j=${arrRow[$i]}
+		if [[ ${dictTicTacToe[$j]} -eq ${dictTicTacToe[$(($j+1))]} ]] && [[ ${dictTicTacToe[$(($j+1))]} -eq ${dictTicTacToe[$(($j+2))]} ]]
+		then 
+		check=0
+		echo "${dictTicTacToe[$j]} is winner"
+		fi
+	done
+}
+
+function checkColumn() {
+	for ((i=0;i<${#arrColumn[*]};i++))
+	do
+		j=${arrColumn[$i]}
+		if [[ ${dictTicTacToe[$j]} -eq ${dictTicTacToe[$(($j+3))]} ]] && [[ ${dictTicTacToe[$(($j+3))]} -eq ${dictTicTacToe[$(($j+6))]} ]]
+		then
+		check=0
+		echo "${dictTicTacToe[$j]} is winner"
+		fi
+	done
+}
+
+function checkDiagonal() {
+	if [[ ${dictTicTacToe[1]} -eq ${dictTicTacToe[5]} ]] && [[ ${dictTicTacToe[5]} -eq ${dictTicTacToe[9]} ]]
+	then
+		check=0
+	elif [[ ${dictTicTacToe[3]} -eq ${dictTicTacToe[5]} ]] && [[ ${dictTicTacToe[5]} -eq ${dictTicTacToe[7]} ]]
+	then
+		check=0
+		echo "${dictTicTacToe[3]} is winner"
+	fi
+
+}
+function checkForTie() {
+	checkTie=$(($checkTie+1))
+		if [[ $checkTie -eq $LAST_POSITION ]]
+		then
+			echo "Game is Tie"
+		fi
+}
+
+function checkWinner(){
+	checkRow
+	checkColumn
+	checkDiagonal
+}
+
 playTicTacToe
